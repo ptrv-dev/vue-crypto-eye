e1605d25e427a5e010ffd1efe304cfaef838bb67
ptrv-dev <petrov99119@gmail.com>
2022-12-11 22:26:40 +0200

    AppBody design

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <app-header />
+  <app-body>
+    <p>Hello here...</p>
+  </app-body>
 </template>
 
 <script>
-import AppHeader from './components/AppHeader.vue';
+import AppHeader from '@/components/AppHeader.vue';
+import AppBody from '@/components/AppBody.vue';
 
 export default {
   components: {
     AppHeader,
+    AppBody,
   },
 };
 </script>
---
+++ src/components/AppBody.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="body">
+    <div class="body__container container">
+      <h2 class="text-shadow">Your watch list:</h2>
+      <div class="body__list">
+        <slot />
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.body {
+  h2 {
+    text-transform: uppercase;
+  }
+  &__container {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+  &__list {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+}
+</style>
